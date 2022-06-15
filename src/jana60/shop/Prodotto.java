63765964581928a2addbdbc9346d2372c35e33c1
Pier95F <forcinapierpaolo@gmail.com>
2022-06-15 11:17:18 +0200

    Inizializzata classe Prodotto

--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -1,5 +1,33 @@
 package jana60.shop;
 
-public class Prodotto {
+import java.text.DecimalFormat;
 
+public class Prodotto {
+	
+	// Attributi
+	String marca, nome;
+	Float prezzoBase;
+	int IVA;
+	
+	DecimalFormat df= new DecimalFormat ("#0.00€"); 
+
+	
+	// Costruttori	
+	public Prodotto (String marca, String nome, Float prezzoBase, int iVA) {
+		super();
+		this.marca = marca;
+		this.nome = nome;
+		this.prezzoBase = prezzoBase;
+		this.IVA = iVA;
+	}
+	
+	
+	// Metodi
+	public float calcolaPrezzoTotale () {
+	return prezzoBase + ((prezzoBase/100) * IVA);
+	}
+	
+	public String toString() {
+		return "Il prezzo finale del prodotto " + nome + "di marca " + marca + "è: " + df.format(calcolaPrezzoTotale());
+	}
 }
